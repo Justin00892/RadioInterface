6ccab06f0ee74c09322113b70d673c5cf8bfecc6
justin00892 <justin00892@gmail.com>
2018-12-11 17:39:15 -0500

    Add project files.

---
+++ RadioInterface/RadioInterface.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace RadioInterface
+{
+    partial class RadioInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.freqBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.freqTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.freqBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Multiselect = true;
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(13, 33);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(91, 23);
+            this.fileButton.TabIndex = 0;
+            this.fileButton.Text = "Upload File";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(110, 33);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.ReadOnly = true;
+            this.fileTextBox.Size = new System.Drawing.Size(184, 22);
+            this.fileTextBox.TabIndex = 1;
+            // 
+            // freqBar
+            // 
+            this.freqBar.Location = new System.Drawing.Point(13, 79);
+            this.freqBar.Maximum = 1100;
+            this.freqBar.Minimum = 800;
+            this.freqBar.Name = "freqBar";
+            this.freqBar.Size = new System.Drawing.Size(282, 56);
+            this.freqBar.TabIndex = 2;
+            this.freqBar.Value = 1065;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1: Select Songs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2: Select Frequency";
+            // 
+            // freqTextBox
+            // 
+            this.freqTextBox.Location = new System.Drawing.Point(206, 56);
+            this.freqTextBox.Name = "freqTextBox";
+            this.freqTextBox.Size = new System.Drawing.Size(88, 22);
+            this.freqTextBox.TabIndex = 5;
+            this.freqTextBox.Text = "106.5";
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(16, 122);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(97, 122);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(220, 122);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // RadioInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 157);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.freqTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.freqBar);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.fileButton);
+            this.Name = "RadioInterface";
+            this.Text = "RadioInterface";
+            ((System.ComponentModel.ISupportInitialize)(this.freqBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.TrackBar freqBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox freqTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ RadioInterface/RadioInterface.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace RadioInterface
+{
+    public partial class RadioInterface : Form
+    {
+        private string[] _files;
+        private string _fileName;
+        private string _piIP = "192.168.1.142";
+        private string _username = "pi";
+        private string _password = "RASPBERRY";
+        private string _directory = "Radio/musicMP3";
+        private SshClient _client;
+        private Thread _radioThread;
+        private double _freq;
+
+        public RadioInterface()
+        {
+            InitializeComponent();
+            freqBar.ValueChanged += (sender, args) =>
+            {
+                var value = (double) freqBar.Value / 10;
+                freqTextBox.Text = value +"";
+            };
+            freqTextBox.TextChanged += (sender, args) =>
+            {
+                int.TryParse(freqTextBox.Text, out var value);
+                value = value * 10;
+                if (value >= 800 && value <= 1100) freqBar.Value = value;
+            };
+        }
+
+        private async void fileButton_Click(object sender, EventArgs e)
+        {
+            fileTextBox.Text = "";
+            var result = openFileDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            if(openFileDialog.SafeFileNames.Length == 1)
+                _fileName = openFileDialog.SafeFileName;
+            foreach (var name in openFileDialog.SafeFileNames)
+            {
+                fileTextBox.Text += "\"" + name +"\" ";
+            }
+
+            _files = openFileDialog.FileNames;
+
+            await Task.Factory.StartNew(UploadFile);
+            startButton.Enabled = true;
+                
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            startButton.Text = "Playing...";
+
+            _freq = (double) freqBar.Value / 10;
+            var newRef = new ThreadStart(SSHToRadio);
+            _radioThread = new Thread(newRef);
+            _radioThread.Start();
+
+            stopButton.Enabled = true;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = false;
+            KillRadio();
+
+            startButton.Text = "Start";
+            startButton.Enabled = true;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            //stops radio
+            if(_radioThread.IsAlive)
+                stopButton_Click(sender,e);
+
+            Application.Restart();
+        }
+
+        private void SSHToRadio()
+        {
+            _client = new SshClient(_piIP,_username,_password);
+            try
+            {
+                _client.Connect();
+                if (_files.Length == 1)
+                    _client.RunCommand("cd Radio; sox -t mp3 ~/Radio/musicMP3/\"" + _fileName +
+                                       "\" -t wav - | sudo ./pi_fm_rds -freq "+ _freq +
+                                       " -ps PirateRadio -audio -");
+                
+                else if (_files.Length > 1)
+                    _client.RunCommand("cd Radio; sox -t mp3 ~/Radio/musicMP3/*.mp3 -t wav - | " +
+                                       "sudo ./pi_fm_rds -freq " + _freq + 
+                                       " -ps PirateRadio -audio -");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void KillRadio()
+        {
+            _client.RunCommand("killall -SIGKILL sox");
+            _radioThread.Abort();
+            _client.Disconnect();
+        }
+
+        private void UploadFile()
+        {
+            var uploadClient = new SftpClient(_piIP,_username,_password);
+            try
+            {
+                uploadClient.Connect();
+                uploadClient.ChangeDirectory(_directory);
+                Console.WriteLine(uploadClient.WorkingDirectory);
+
+                foreach (var file in _files)
+                {
+                    using (var fileStream = new FileStream(file, FileMode.Open))
+                    {
+                        uploadClient.BufferSize = 4 * 1024;
+                        uploadClient.UploadFile(fileStream, Path.GetFileName(file));
+                    }
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                uploadClient.Disconnect();
+            }
+            
+        }
+    }
+}
